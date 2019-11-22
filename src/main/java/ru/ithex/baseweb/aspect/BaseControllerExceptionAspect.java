c01066dfa172a59a9593cc28ebc337154c2242c3
Andrey Chuchalin <chuchalin92@mail.ru>
2019-11-22 12:58:10 +0300

    Update

--- src/main/java/ru/ithex/baseweb/aspect/BaseControllerExceptionAspect.java
+++ src/main/java/ru/ithex/baseweb/aspect/BaseControllerExceptionAspect.java
@@ -11,22 +11,22 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.stream.Collectors;
 
-public class BaseControllerExceptionAspect {
+public abstract class BaseControllerExceptionAspect {
     private final static Logger LOGGER = LoggerFactory.getLogger("");
 
     @Pointcut()
-    public void controllersAdvicePackagePointcut(){}
+    public abstract void controllersAdvicePackagePointcut();
 
-    @Before("controllersAdvicePackagePointcut()")
+    @Before("controllersAdvicePackagePointcut() || execution(* ru.ithex.baseweb.controller.advice.*.*(..))")
     private void controllerAdviceAspect(JoinPoint joinPoint) {
         final StringBuilder stringBuilder = new StringBuilder("Request error:\n"); //String.format("%s:\n", joinPoint.getSignature().getDeclaringTypeName())
         Object[] args = joinPoint.getArgs();
         Arrays.stream(args).filter(o -> o instanceof HttpServletRequest)
                 .map(o -> (HttpServletRequest) o)
                 .forEach(request -> {
-                    stringBuilder.append(String.format("%s %s\nHeaders:\n", request.getMethod(), request.getRequestURL()));
+                    stringBuilder.append(String.format("%s %s\nHeaders:", request.getMethod(), request.getRequestURL()));
                     Collections.list(request.getHeaderNames()).forEach(name -> {
-                        stringBuilder.append(String.format("    %s: %s\n", name, request.getHeader(name)));
+                        stringBuilder.append(String.format("\n    %s: %s", name, request.getHeader(name)));
                     });
                     stringBuilder.append("\nBody:");
                     try{
